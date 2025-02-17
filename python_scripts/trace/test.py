0e7294715b1667b14ffff1034bc274286ffddb1a
Aleksey Loktionov <sams12.34.58@gmail.com>
2025-02-17 19:27:43 +0500

    change test.py

--- python_scripts/trace/test.py
+++ python_scripts/trace/test.py
@@ -6,6 +6,11 @@ from utils import start, finish
 drone = pioneer_sdk.Pioneer()
 start(drone)
 for array in map:
-    drone.go_to_local_point_body_fixed(x = array[0], y = array[1], z = array[2], yaw = 0)
+    if array == 'arm':
+        start(drone)
+    elif array == 'land':
+        finish(drone)
+    else:
+        drone.go_to_local_point_body_fixed(x = array[0], y = array[1], z = array[2], yaw = 0)
     time.sleep(3)
 finish(drone)
